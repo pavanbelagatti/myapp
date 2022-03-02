107dd02054343352b23f88432012ff8039ae4d14
pavanbelagatti <pavbelnsb@gmail.com>
2022-03-02 12:29:12 +0530

    First commit

--- app.js
+++ app.js
@@ -7,3 +7,17 @@ it('Main page content', function(done) {
         done();
     });
 });
+
+it('Main page status', function(done) {
+    request('http://localhost:8080' , function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+    });
+});
+
+it('About page content', function(done) {
+    request('http://localhost:8080/about' , function(error, response, body) {
+        expect(response.statusCode).to.equal(404);
+        done();
+    });
+});
